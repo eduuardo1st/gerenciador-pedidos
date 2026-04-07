65e22a3cfd8faf6e1519f0e816ceb3a6e896bb01
Eduardo Gonçalves <eduardouc388@gmail.com>
2026-04-07 18:40:08 -0300

    Update

--- gerenciador-pedidos/src/main/java/com/eduuardo1st/gerenciador_pedidos/model/Produto.java
+++ gerenciador-pedidos/src/main/java/com/eduuardo1st/gerenciador_pedidos/model/Produto.java
@@ -1,4 +1,38 @@
 package com.eduuardo1st.gerenciador_pedidos.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true, nullable = false)
+    private String nome;
+    @Column(name = "valor")
+    private Double preco;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
 }
