8d12e9c153d37879bf3419afc9a7e094f4574dab
Eduardo Gonçalves <eduardouc388@gmail.com>
2026-04-09 17:52:50 -0300

    Update

--- gerenciador-pedidos/src/main/java/com/eduuardo1st/gerenciador_pedidos/GerenciadorPedidosApplication.java
+++ gerenciador-pedidos/src/main/java/com/eduuardo1st/gerenciador_pedidos/GerenciadorPedidosApplication.java
@@ -4,6 +4,7 @@ import com.eduuardo1st.gerenciador_pedidos.model.Categoria;
 import com.eduuardo1st.gerenciador_pedidos.model.Pedido;
 import com.eduuardo1st.gerenciador_pedidos.model.Produto;
 import com.eduuardo1st.gerenciador_pedidos.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,11 @@ import java.time.LocalDate;
 
 @SpringBootApplication
 public class GerenciadorPedidosApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository cRepository;
+    @Autowired
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorPedidosApplication.class, args);
@@ -25,7 +31,6 @@ public class GerenciadorPedidosApplication implements CommandLineRunner {
         Produto produto = new Produto("Carne", 50.0);
         Pedido pedido = new Pedido(LocalDate.now());
 
-        CategoriaRepository CRepository = null;
-
+        cRepository.save(categoria);
     }
 }
